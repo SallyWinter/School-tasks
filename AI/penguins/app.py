85ff12cbb06f0736078fb42d902158dcd24a5ba7
Sally <88275731+SallyWinter@users.noreply.github.com>
2023-09-01 09:53:37 +0200

    Pengu page

--- AI/penguins/app.py
+++ AI/penguins/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 import pickle
 import numpy as np
-
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import accuracy_score
@@ -13,18 +12,15 @@ warnings.warn = lambda *args, **kwargs: 1+1
 
 app = Flask(__name__)
 
-with open('model.pkl', 'rb') as f:
-    model = pickle.load(f)
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
 
 @app.route('/')
 def hello_world():
-    print(request.remote_addr)
     return render_template("template.html")
 
 @app.route("/submit", methods=["POST"])
 def submit():
-    if request.remote_addr == "10.128.130.251":
-        return jsonify({"status": "error", "value": "Blocked IP"})
 
     data = np.array([request.json["data"]])
     print(data)
@@ -33,8 +29,7 @@ def submit():
     prediction = [
         "Adelie Penguin (Pygoscelis adeliae)", 
         "Gentoo penguin (Pygoscelis papua)", 
-        "Chinstrap penguin (Pygoscelis antarctica)"
-    ][int(prediction)]
+        "Chinstrap penguin (Pygoscelis antarctica)"] [int(prediction)]
 
     print(prediction)
 
